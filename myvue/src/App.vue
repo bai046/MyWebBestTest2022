13d8222fe7e44be903753274f5093a035a50c743
shuying Guo <70828681+XiaoBai918@users.noreply.github.com>
2021-05-16 23:47:47 +0800

    1

--- myvue/src/App.vue
+++ myvue/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <HelloWorld/>
+    <router-view></router-view>
+    <HelloWorld />
     
   </div>
 </template>
--- myvue/src/main.js
+++ myvue/src/main.js
@@ -1,16 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import $ from 'jquery'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
 import VueRouter from 'vue-router'
 // 引入组件
 import User from './components/user.vue'
+// 引入组件
+import One from './components/one.vue'
+import Two from './components/two.vue'
+
 const routes = [
   {
       path: "/user",
       component: User
-  }
+  },
+  {
+    path: "/one",
+    component: One
+},
+{
+    path: "/two",
+    component: Two
+}
 ]
 Vue.use(VueRouter)
 // 创建 router 实例，然后传 `routes` 配置
